aa83126bdf3db44fa79803f8a5939a90be9a4f16
Emmanuel Ifeanyi MECHIE <64092981+emmanuelByte@users.noreply.github.com>
2026-09-07 17:55:44 +0100

    feat(tts): register local TTS proxy

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,11 @@ import { aiAssistPlugin } from './server/aiAssistPlugin'
 import { assetStoragePlugin } from './server/assetStoragePlugin'
 import { exportPlugin } from './server/exportPlugin'
 import { libraryStoragePlugin } from './server/libraryStoragePlugin'
+import { localTtsPlugin } from './server/localTtsPlugin'
 import { projectStoragePlugin } from './server/projectStoragePlugin'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss(), aiAssistPlugin(), projectStoragePlugin(), assetStoragePlugin(), libraryStoragePlugin(), exportPlugin()],
+  plugins: [react(), tailwindcss(), aiAssistPlugin(), projectStoragePlugin(), assetStoragePlugin(), libraryStoragePlugin(), localTtsPlugin(), exportPlugin()],
   server: {
     port: 3000,
   }
